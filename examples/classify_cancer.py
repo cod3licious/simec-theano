mainly bug fix: simmat was modified inplace by similarity encoder when scaling to max value 1

examples/classify_cancer.py
@@ -5,11 +5,10 @@ import sklearn.metrics as skmet
 
 # https://github.com/cod3licious/nlputils
 from nlputils.features import FeatureTransform, features2mat
-from nlputils.dict_utils import select_copy
 from simec.similarity_encoder import SimilarityEncoder, scaled_sigmoid
 
 # https://github.com/cod3licious/cancer_papers
-from cancer_papers.load_cancer import articles2dict
+from datasets.cancer_papers.load_cancer import articles2dict
 
 
 def classify_sklearn(X, X_test, y, y_test):
@@ -30,14 +29,14 @@ if __name__ == '__main__':
     # split in train and test dataset + get label vectors
     np.random.seed(42)
     doc_ids = list(np.random.permutation(sorted(textdict.keys())))[:10000]
-    textdict = select_copy(textdict, doc_ids)
+    textdict = {d: textdict[d] for d in doc_ids}
     train_ids = doc_ids[:int(.8 * len(doc_ids))]
     test_ids = doc_ids[int(.8 * len(doc_ids)):]
     y_train_k = np.array([doccats_k[i] for i in train_ids])
     y_train_p = np.array([doccats_p[i] for i in train_ids])
     y_test_k = np.array([doccats_k[i] for i in test_ids])
     y_test_p = np.array([doccats_p[i] for i in test_ids])
-    print "%i training documents and %i test documents" % (len(train_ids), len(test_ids))
+    print "[INFO] %i training documents and %i test documents" % (len(train_ids), len(test_ids))
 
     # compute length renormed tf-idf features and analyze
     print "[INFO] tf-idf with **length** renorm"
@@ -45,7 +44,7 @@ if __name__ == '__main__':
     docfeats = ft.texts2features(textdict, train_ids)
     print "[INFO] train matrix...",
     X, featurenames = features2mat(docfeats, train_ids)
-    print "test matrix..."
+    print "with %i features ...test matrix..." % len(featurenames)
     X_test, _ = features2mat(docfeats, test_ids, featurenames)
     # classify
     print "[INFO] classify based on **keyword**"

examples/utils.py
@@ -1,9 +1,9 @@
 import colorsys
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import offsetbox
 from scipy.spatial.distance import pdist
+from scipy.stats import spearmanr
 from sklearn.datasets import make_circles, make_blobs, make_swiss_roll, make_s_curve
 from sklearn.utils import check_random_state
 
@@ -210,3 +210,30 @@ def check_embed_match(X_embed1, X_embed2):
     D_emb1 /= D_emb1.max()
     D_emb2 /= D_emb2.max()
     return np.corrcoef(D_emb1, D_emb2)[0, 1]
+
+
+def check_similarity_match(X_embed, S):
+    """
+    Since SimEcs are supposed to project the data into an embedding space where the target similarities
+    can be linearly approximated, check if X_embed*X_embed^T = S
+    (check mean squared error and Spearman correlation coefficient)
+    Inputs:
+        - X_embed: Nxd matrix with coordinates in the embedding space
+        - S: NxN matrix with target similarities (do whatever transformations were done before using this
+             as input to the SimEc, e.g. centering, etc.)
+    Returns:
+        - msq, rho: mean squared error and Spearman correlation coefficent between linear kernel of embedding
+                    and target similarities (mean squared error is more exact, corrcoef a more relaxed error measure)
+    """
+    # compute linear kernel as approximated similarities
+    S_approx = X_embed.dot(X_embed.T)
+    # to get results that are comparable across similarity measures, we have to normalize them somehow,
+    # in this case by dividing by the absolute max value of both similarity matrices
+    n = max(np.abs(S_approx).max(), np.abs(S).max())
+    S /= n
+    S_approx /= n
+    # compute mean squared error
+    msq = np.mean((S - S_approx) ** 2)
+    # compute Spearman correlation coefficient
+    rho = spearmanr(S, S_approx, axis=None)[0]
+    return msq, rho

similarity_encoder.py
@@ -218,7 +218,7 @@ class SimilarityEncoder(object):
         assert S.shape[1] == self.n_targets, "wrong number of targets specified when initializing the model"
         # normalize similarity matrix, other wise the weights will overshoot (turn
         # to nan) / we would have to be too careful with the learning rate
-        S /= np.max(np.abs(S))
+        S = S/np.max(np.abs(S))
         # define some variables for training
         n_train = X.shape[0]
         # work on 20 training examples at a time
@@ -282,7 +282,7 @@ class SimilarityEncoder(object):
         for i, l in enumerate(best_layers):
             self.model.layers[i].W.set_value(l.W.get_value(borrow=False))
             self.model.layers[i].b.set_value(l.b.get_value(borrow=False))
-        print("Final training error: %.10f; lowest error: %.10f" % (mean_train_error[-1], best_error))
+        print("Final training error after %i epochs: %.10f; lowest error: %.10f" % (e, mean_train_error[-1], best_error))
         # one last time just to get the error to double check
         test_error = []
         for bi in range(n_batches):